abcfc4115c5d5c6f907d17ee938453e15f7cb1f7
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-25 02:53:56 +0530

    Add part time

--- src/com/empwageoops/EmpWageComputation.java
+++ src/com/empwageoops/EmpWageComputation.java
@@ -3,27 +3,22 @@ package com.empwageoops;
 public class EmpWageComputation {
 	// Creating a method
 	public void welcomeMsg() {
-
 		// Check the employee is present or absent
-		int IS_FULL_TIME = 1;
+		int Is_Part_Time = 1; // Added part time condition
+		int IS_FULL_TIME = 2;
 		// Calculating daily employee wage
 		final int empWagePerHrs = 20;
 		int empHrs = 0;
 		int empWage = 0;
-
-		double empcheck = Math.floor(Math.random() * 10) % 2;
-		if (empcheck == IS_FULL_TIME) {
-
+		double empcheck = Math.floor(Math.random() * 10) % 3; // Check random value between 0,1 and 2
+		if (empcheck == Is_Part_Time) {
 			empHrs = 8;
-			System.out.println("emp is present");
-		} else {
-
+		} else if (empcheck == IS_FULL_TIME) {
+			empHrs = 4; //
+		} else
 			empHrs = 0;
-			System.out.println("emp is not present");
-		}
 		empWage = empHrs * empWagePerHrs;
 		System.out.println("EmpWage :" + " " + empWage);
-
 	}
 
 	public static void main(String[] args) {
