65d86a1e81489b018a096c547191a9ad34eb27d9
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-25 02:03:53 +0530

    Welcome employee wage computation problem

---
+++ src/com/empwageoops/EmpWageComputation.java
@@ -0,0 +1,17 @@
+package com.empwageoops;
+
+public class EmpWageComputation {
+	// creating a method
+	public void welcomeMsg() {
+		System.out.println("Welcome employee wage computation problem");
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EmpWageComputation ewc=new EmpWageComputation();
+		ewc.welcomeMsg();
+
+	}
+
+}
