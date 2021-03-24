9dbd6f48351f00277f23af7f9fba5f65aea878d4
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-25 02:10:16 +0530

    Employee is present or absent

--- src/com/empwageoops/EmpWageComputation.java
+++ src/com/empwageoops/EmpWageComputation.java
@@ -1,15 +1,25 @@
 package com.empwageoops;
 
 public class EmpWageComputation {
-	// creating a method
+	// Creating a method
 	public void welcomeMsg() {
 		System.out.println("Welcome employee wage computation problem");
-		
+
+		// Check the employee is present or absent
+		int IS_FULL_TIME = 1;
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+		if (empcheck == IS_FULL_TIME) {
+			System.out.println("emp is present");
+		} else {
+			System.out.println("emp is not present");
+		}
+
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		EmpWageComputation ewc=new EmpWageComputation();
+		// To creating a object and call the method
+		EmpWageComputation ewc = new EmpWageComputation();
 		ewc.welcomeMsg();
 
 	}
