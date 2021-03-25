ed0383f0066b1cf98d8da7b283fee8c0949133e6
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-25 13:28:10 +0530

    using switch case solve the problem

--- src/com/empwageoops/EmpWageComputation.java
+++ src/com/empwageoops/EmpWageComputation.java
@@ -4,8 +4,8 @@ public class EmpWageComputation {
 	// Creating a method
 	public void welcomeMsg() {
 		// Check the employee is present or absent
-		int Is_Part_Time = 1; // Added part time condition
-		int IS_FULL_TIME = 2;
+		final int Is_Part_Time = 1; // Added part time condition
+		final int IS_FULL_TIME = 2;
 		// Calculating daily employee wage
 		final int empWagePerHrs = 20;
 		int empHrs = 0;
@@ -19,7 +19,23 @@ public class EmpWageComputation {
 			empHrs = 0;
 		empWage = empHrs * empWagePerHrs;
 		System.out.println("EmpWage :" + " " + empWage);
+		
+		int empCheck=(int)Math.floor(Math.random() *10) %3 ;
+		switch (empCheck) {
+		case Is_Part_Time:
+			empHrs=4;
+			break;
+		case IS_FULL_TIME:
+			empHrs=8;
+			break;
+		default:
+			empHrs=0;
+		}
+		empWage=empHrs*empHrs;
+		System.out.println("Emp Wage"+ " "+ empWage);
+
 	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
